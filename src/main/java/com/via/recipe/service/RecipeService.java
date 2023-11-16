abaa29dd974fa5368e0cf0a9cb819cca9bf42ec3
Valentin Alberto <valalberto0214@gmail.com>
2023-11-16 12:40:22 +0800

    Create Recipe Service layer
    add recipe service implementation

---
+++ src/main/java/com/via/recipe/service/RecipeService.java
@@ -0,0 +1,22 @@
+/**
+ * Licensed Materials - Property of hunchmen
+ * 
+ * (C) hunchmen. 2023. All Rights Reserved.
+ * 
+ */
+package com.via.recipe.service;
+
+import java.util.List;
+import com.via.recipe.model.Recipe;
+
+/**
+ * 
+ * @author via
+ * 
+ * @date 16 Nov 2023
+ *
+ */
+public interface RecipeService {
+
+    List<Recipe> getAllRecipes();
+}
---
+++ src/main/java/com/via/recipe/service/RecipeServiceImpl.java
@@ -0,0 +1,40 @@
+/**
+ * Licensed Materials - Property of hunchmen
+ * 
+ * (C) hunchmen. 2023. All Rights Reserved.
+ * 
+ */
+package com.via.recipe.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.via.recipe.model.Recipe;
+import com.via.recipe.repository.RecipeRepository;
+
+/**
+ * 
+ * @author via
+ * 
+ * @date 16 Nov 2023
+ *
+ */
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.via.recipe.service.RecipeService#getAllRecipes()
+     */
+    @Override
+    public List<Recipe> getAllRecipes() {
+        return recipeRepository.findAll();
+    }
+
+}
